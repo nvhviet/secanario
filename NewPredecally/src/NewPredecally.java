149c8ed6c42e2d8d02c60d5c8418fcca35d4f5b0
tmgiang <tmgiang@spsvietnam.vn>
2014-04-15 13:12:08 +0700

    new main methods

---
+++ NewPredecally/src/NewPredecally.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author tmgiang
+ *
+ */
+public class NewPredecally {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
