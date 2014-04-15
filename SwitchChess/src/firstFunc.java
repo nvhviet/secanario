834d095b6d2614eb7ded8979527a3989f88f25e7
tmgiang <tmgiang@spsvietnam.vn>
2014-04-15 10:07:03 +0700

    Check Commit

---
+++ SwitchChess/src/firstFunc.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author tmgiang
+ *
+ */
+public class firstFunc {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
